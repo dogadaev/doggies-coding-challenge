50315d12a7fa0472c2b03c06408c2c04b12aa5ab
Vlad Dogadaev <vmdogad@gmail.com>
2023-08-21 20:02:47 +0200

    inital commit

---
+++ app/src/main/java/org/dogadaev/simplesurance_dogs_collection/MainActivity.kt
@@ -0,0 +1,46 @@
+package org.dogadaev.simplesurance_dogs_collection
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.dogadaev.simplesurance_dogs_collection.ui.theme.SimplesurancedogscollectionTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SimplesurancedogscollectionTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SimplesurancedogscollectionTheme {
+        Greeting("Android")
+    }
+}
