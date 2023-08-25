86220d55b0469f2ca05606711cd0f2722ebf1c19
Vlad Dogadaev <vmdogad@gmail.com>
2023-08-25 05:07:48 +0200

    chore(styling): adjusted typography

--- app/src/main/java/org/dogadaev/simplesurance_dogs_collection/ui/theme/Theme.kt
+++ app/src/main/java/org/dogadaev/simplesurance_dogs_collection/ui/theme/Theme.kt
@@ -38,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun SimplesurancedogscollectionTheme(
+fun SimplesuranceDogsCollection(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
